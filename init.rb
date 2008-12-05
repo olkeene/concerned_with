5dcc2d8fb6831c25d8e26ba5ee1f9c644559a4df
unknown <ol.keene@gmail.com>
2008-12-05 17:13:05 +0200

    Added support for controllers concerning with example

--- init.rb
+++ init.rb
@@ -1,3 +1,4 @@
 # Include hook code here
 require File.dirname(__FILE__) + '/lib/concerned_with'
-ActiveRecord::Base.send(:extend, ConcernedWith)
+ActiveRecord::Base.send     :extend, ConcernedWith
+ActionController::Base.send :extend, ConcernedWith
